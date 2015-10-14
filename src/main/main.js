b9b06a8a010a8cd8ce5f266b21b1cdd605069aa5
Tom Bigelajzen <tombigel@wix.com>
2015-10-14 18:53:13 +0300

    add test setup

--- src/main/main.js
+++ src/main/main.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const config = require('./config');
-
 const app = express();
 
+function verifyIfttt(req, res, next) {
+    var channelKey = req.get('IFTTT-Channel-Key');
+    if (channelKey && channelKey === process.env.channelKey) {
+        next();
+    } else {
+        res.status(401).send('Unknown Source');
+    }
+}
+
+function postTestSetup(req, res) {
+    res.status(200).json({data: {samples: {}}});
+}
 
 app.get('/', (req, res) => res.status(200).send('OK'));
-app.get('/ifttt/v1/status', (req, res) => res.status(200).end());
+app.get('/ifttt/v1/status', verifyIfttt, (req, res) => res.status(200));
+app.post('/ifttt/v1/test/setup', verifyIfttt, postTestSetup);
 
 var server = app.listen(config.port, () => {
     console.log('Now listening to http://%s:%s,', server.address().address, server.address().port);
