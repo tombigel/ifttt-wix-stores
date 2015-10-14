66c49a07d27b6f47032d07b98ffc1e0b6695ccf8
Omer Ganim <ganimomer@gmail.com>
2015-10-14 18:17:45 +0300

    add default request

--- src/main/main.js
+++ src/main/main.js
@@ -1,48 +1,12 @@
 const express = require('express');
 const config = require('./config');
+
 const app = express();
 
-app.get('/ifttt/v1/status', getStatus);
 
-function getStatus(req, res){
-    res.status(200).end();
-}
+app.get('/', (req, res) => res.status(200).send('OK'));
+app.get('/ifttt/v1/status', (req, res) => res.status(200).end());
 
 var server = app.listen(config.port, () => {
     console.log('Now listening to http://%s:%s,', server.address().address, server.address().port);
 });
-
-
-//function requestPromise(url, instance) {
-//  return new Promise(function (resolve, reject) {
-//    request.get({
-//      url: url,
-//      headers: {
-//        'Access-Control-Allow-Origin': '*',
-//        'X-ecom-instance': instance
-//      }
-//    }, function (err, res, body) {
-//      if (err) {
-//        reject(err);
-//      } else {
-//        resolve(body)
-//      }
-//    });
-//  })
-//}
-//
-//
-//var products;
-//var categories;
-//requestPromise(PREFIX + '/get-category-list', INSTANCE)
-//  .then(JSON.parse)
-//  .then(data => console.log(data))//categories = data.categories);
-//
-////console.log('Hello world!'); //eslint-disable-line
-////setInterval(function(){
-////  _.forEach(categories, function(cat) {
-////    requestPromise(PREFIX + '/get-category-items?categoryId=' + cat.id, INSTANCE)
-////    .then(JSON.parse)
-////    .then(data => console.log(JSON.stringify(data, null, 2)))
-////  })
-////}, 1000 * 10);
