38922dcb3fcc63693dc541b86433722e5cad9460
Omer Ganim <ganimomer@gmail.com>
2015-10-15 12:21:18 +0300

    change flow of base address to include version

--- src/main/config.js
+++ src/main/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     port: process.env.PORT || 5000,
     channelKey: process.env.channelKey,
-    firebaseApp: process.env.firebaseApp
+    firebaseApp: process.env.firebaseApp,
+    HEROKU_RELEASE_VERSION: process.env.HEROKU_RELEASE_VERSION
 };
