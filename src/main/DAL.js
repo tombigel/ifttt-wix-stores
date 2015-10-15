ef2726e648e83f851856c79a1b1e0c6a18384945
Omer Ganim <ganimomer@gmail.com>
2015-10-15 13:22:53 +0300

    add more logs

--- src/main/DAL.js
+++ src/main/DAL.js
@@ -5,7 +5,10 @@ let instances;
 
 function promiseValues(instance) {
   return new Promise(function (resolve) {
-    instances.once(instance, (data) => resolve(data));
+    instances.once(instance, function (data) {
+      console.log(`requested data for instance ${instance} with data ${JSON.parse(data)}`);
+      resolve(data)
+    });
   });
 }
 
