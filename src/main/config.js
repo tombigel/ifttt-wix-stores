2d72a7ffc0156d3b10946a2c72ca1577092d0cea
Omer Ganim <ganimomer@gmail.com>
2015-10-15 12:19:59 +0300

    remove storeInstance from config

--- src/main/config.js
+++ src/main/config.js
@@ -1,6 +1,5 @@
 module.exports = {
     port: process.env.PORT || 5000,
-    storeInstance: process.env.storeInstance,
     channelKey: process.env.channelKey,
     firebaseApp: process.env.firebaseApp
 };
