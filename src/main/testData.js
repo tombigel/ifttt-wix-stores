651832cfa71e6a8b57bd106e3aa88849573a5a34
Omer Ganim <ganimomer@gmail.com>
2015-10-15 11:02:31 +0300

    add promise.catch

--- src/main/testData.js
+++ src/main/testData.js
@@ -5,7 +5,7 @@ module.exports = {
     samples: {
       triggers: {
         new_product_added: {
-          store_id: '1'
+          store_id: 'mii3FCjaPiaVeYuV5Er788nOuhk8EXSPRGpHf6fMNDo.eyJpbnN0YW5jZUlkIjoiMTNmZGE4ZGEtYThmNy0yZjRkLWE1NmMtMDYyYTk1MDU0MTE5Iiwic2lnbkRhdGUiOiIyMDE1LTEwLTEzVDA4OjQ1OjE5LjMyMloiLCJ1aWQiOiIzZjljMzQ3ZS04M2I2LTQ4OWItYTdiMS00ZDZlMmNmMGJhMzgiLCJwZXJtaXNzaW9ucyI6Ik9XTkVSIiwiaXBBbmRQb3J0IjoiOTEuMTk5LjExOS4xNC80MTAxNSIsInZlbmRvclByb2R1Y3RJZCI6bnVsbCwiZGVtb01vZGUiOmZhbHNlLCJvcmlnaW5JbnN0YW5jZUlkIjoiMTNjMzY0ODEtYjhlNC0wNWFhLWVlZmMtNjEyYTZiNWIzZTAzIiwiYmlUb2tlbiI6Ijg0OWZmZDc0LWY0NDItNmE1ZS0xYTg0LWQxMDhhNmE2ZTE5MiIsInNpdGVPd25lcklkIjoiM2Y5YzM0N2UtODNiNi00ODliLWE3YjEtNGQ2ZTJjZjBiYTM4In0'
         }
       }
     }
