4d1b249f94b752b0721ad67a4e4e8d1ba858bb5c
Omer Ganim <ganimomer@gmail.com>
2015-10-15 12:58:10 +0300

    add error handling to wixStores

--- src/main/wixStores.js
+++ src/main/wixStores.js
@@ -7,7 +7,9 @@ const _ = require('lodash');
 const wixStores = require('./wixStoresFacade');
 
 function getNewProducts(instance) {
-  return Promise.all([DAL.getProducts(instance), wixStores.pollProducts(instance)])
+  const dalPromise = DAL.getProducts(instance).catch(()=> Error('Firebase Error'));
+  const wixStoresPromise = wixStores.pollProducts(instance).catch(()=> Error('Stores API Error'));
+  return Promise.all([dalPromise, wixStoresPromise])
     .then(function (data) {
       const pastProducts = data[0];
       const products = data[1].products.map(function (product) {
